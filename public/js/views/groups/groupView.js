8e9ee30946b21dacc4866ec8fdc5ca738275db7d
Pablo Lopez <pablo.lopez.santori@gmail.com>
2017-11-21 22:04:06 +0100

    Individual model-views listen for individual changes.

--- public/js/views/groups/groupView.js
+++ public/js/views/groups/groupView.js
@@ -14,14 +14,17 @@ $(function () {
     },
 
     initialize: function () {
-
+      this.listenTo(this.model, 'change', this.render)
     },
 
     render: function () {
+      let isOnline = (this.model.get('activeUsers').length > 0 ? true : false);
+
       let template = Handlebars.compile(this.template);
       let html = template(this.model.toJSON());
 
       this.$el.html(html);
+      this.$('.image-animation').toggleClass('online-group', isOnline)
       return this;
     },
 
