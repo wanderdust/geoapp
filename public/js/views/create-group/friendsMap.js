8edc4e63eae3e39225763a4d580f691010d6d1d3
Pablo <pablo.lopez.santori@gmail.com>
2018-01-13 13:44:44 +0000

    Added search box in create-group view

--- public/js/views/create-group/friendsMap.js
+++ public/js/views/create-group/friendsMap.js
@@ -16,6 +16,7 @@ $(function () {
 
       this.getUserLocation();
       this.currentMarkers = [];
+
     },
 
     render: function () {
@@ -38,9 +39,58 @@ $(function () {
           that.placeMarker(e.latLng, map);
           coords.lat = e.latLng.lat();
           coords.lng = e.latLng.lng();
-          app.userCollection.trigger('groupCoords', coords)
-      })
+          app.userCollection.trigger('groupCoords', coords);
+      });
+
+      this.searchbox(map);
     },
+
+    searchbox: function (map) {
+      let that = this;
+      // Create the search box and link it to the UI element.
+      let input = document.getElementById('pac-input');
+      let searchBox = new google.maps.places.SearchBox(input);
+      map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+      // Bias the SearchBox results towards current map's viewport.
+      map.addListener('bounds_changed', function() {
+        searchBox.setBounds(map.getBounds());
+      });
+
+      searchBox.addListener('places_changed', function() {
+        let places = searchBox.getPlaces();
+        let place = places[0];
+        let coords = {
+          lat: place.geometry.location.lat(),
+          lng: place.geometry.location.lng()
+        }
+
+        if (places.length == 0) {
+          return;
+        }
+
+        // For each place, get the icon, name and location.
+        let bounds = new google.maps.LatLngBounds();
+        if (!place.geometry) {
+          console.log("Returned place contains no geometry");
+          return;
+        };
+
+        // Create a marker for each place.
+        // Clear out the old markers.
+        that.placeMarker(place.geometry.location, map);
+        app.userCollection.trigger('groupCoords', coords);
+
+        if (place.geometry.viewport) {
+          // Only geocodes have viewport.
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+        }
+        map.fitBounds(bounds);
+      });
+    },
+
     // Gets the user current location to set as center of map.
     getUserLocation: function () {
       let that = this;
@@ -57,21 +107,25 @@ $(function () {
         that.initMap(coords)
       });
     },
+
     // Places a marker.
     placeMarker: function(position, map) {
       this.removeMarkers();
-        var marker = new google.maps.Marker({
-            position: position,
-            map: map
-        });
-        map.panTo(position);
-        this.currentMarkers.push(marker)
+      let marker = new google.maps.Marker({
+          position: position,
+          map: map
+      });
+      map.panTo(position);
+      this.currentMarkers.push(marker);
+      console.log(this.currentMarkers)
     },
+
     // Removes all markers.
     removeMarkers: function () {
       for(let i = 0; i < this.currentMarkers.length; i++){
         this.currentMarkers[i].setMap(null);
       }
+      this.currentMarkers = [];
     },
 
   })
