eeb9c677475ee15ee6625131b3eac7d87a588eca
pablo <pablo.lopez.santori@gmail.com>
2018-01-25 18:19:01 +0000

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 let mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/GeoApp', { useMongoClient: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/GeoApp', { useMongoClient: true });
 mongoose.Promise = global.Promise;
 
 module.exports = {mongoose}
